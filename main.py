be2c97381da7370383ce297c0007e63e5483108e
Zakariyya Scavotto <44652622+ZakariyyaScavotto@users.noreply.github.com>
2023-06-05 21:44:49 -0400

    Reworked how the econ data is read and lagged in the code, created pre-2020 and 2020-beyond train/test/split

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
 from sklearn.preprocessing import StandardScaler
-
 # import to hide the annoying warnings about numba.jit when importing shap library
 import warnings
 warnings.filterwarnings("ignore", message=".*The 'nopython' keyword.*")
@@ -14,30 +13,54 @@ import matplotlib.pyplot as plt
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.models import load_model
+from sklearn.linear_model import Lasso
 
 def readEconData(filename):
-    df = pd.read_excel(filename)
-    # Last number in HousingPriceInd missing so dropping that row
-    df.dropna(subset=["HPI%Change"], inplace=True)
-    df.drop("Date", axis=1, inplace=True)
-    # print(df.isna().sum())
-    return df
+    return pd.read_excel(filename)
 
-def makeTrainTest(econData):
+def makeTrainTest(): # Train test but with breaking up between pre-2020 and 2020->beyond
+    # Read econ data
+    econData = readEconData("Data\ConstructedDataframes\ALLECONDATAwithLags.xlsx")
+    # split into pre-2020 and 2020->beyond
+    ind2020 = econData.query("Date == '2020-01-01'").index.values[0]
+    pre2020EconData, post2020EconData = econData.iloc[:ind2020].copy(), econData.iloc[ind2020:].copy()
+    # now, can remove the date column
+    pre2020EconData.drop("Date", axis=1, inplace=True)
+    post2020EconData.drop("Date", axis=1, inplace=True)
+    # now scale the data
+    scaler, scaler2 = StandardScaler(), StandardScaler()
+    pre2020EconData = pd.DataFrame(scaler.fit_transform(pre2020EconData), columns=pre2020EconData.columns)
+    post2020EconData = pd.DataFrame(scaler2.fit_transform(post2020EconData), columns=post2020EconData.columns)
+    # now need to split each into train/test
+    # split pre-2020
+    trainCutoff = int(pre2020EconData.shape[0] * 0.8) 
+    pre2020TrainDf, pre2020TestDf = pre2020EconData.iloc[:trainCutoff], pre2020EconData.iloc[trainCutoff:]
+    pre2020xTrain, pre2020yTrain = pre2020TrainDf.loc[:, pre2020TrainDf.columns != "AnnualizedMoM-CPI-Inflation"], pre2020TrainDf.loc[:, pre2020TrainDf.columns == "AnnualizedMoM-CPI-Inflation"]
+    pre2020xTest, pre2020yTest = pre2020TestDf.loc[:, pre2020TestDf.columns != "AnnualizedMoM-CPI-Inflation"], pre2020TestDf.loc[:, pre2020TestDf.columns == "AnnualizedMoM-CPI-Inflation"]
+    # Now do the same for post2020EconData
+    trainCutoff = int(post2020EconData.shape[0] * 0.8)
+    post2020TrainDf, post2020TestDf = post2020EconData.iloc[:trainCutoff], post2020EconData.iloc[trainCutoff:]
+    post2020xTrain, post2020yTrain = post2020TrainDf.loc[:, post2020TrainDf.columns != "AnnualizedMoM-CPI-Inflation"], post2020TrainDf.loc[:, post2020TrainDf.columns == "AnnualizedMoM-CPI-Inflation"]
+    post2020xTest, post2020yTest = post2020TestDf.loc[:, post2020TestDf.columns != "AnnualizedMoM-CPI-Inflation"], post2020TestDf.loc[:, post2020TestDf.columns == "AnnualizedMoM-CPI-Inflation"]
+    return pre2020xTrain, pre2020yTrain, pre2020xTest, pre2020yTest, post2020xTrain, post2020yTrain, post2020xTest, post2020yTest
+
+def makeTrainTestOld():
     # Split the data into training and testing (80/20) while keeping time-order
+    # Read econ data
+    econData = readEconData("Data\ConstructedDataframes\ALLECONDATAwithLags.xlsx")
     scaler = StandardScaler()
+    if "Date" in econData.columns:
+        econData.drop("Date", axis=1, inplace=True)
     econData = pd.DataFrame(scaler.fit_transform(econData), columns=econData.columns)
-    # print(econData.head())
-    trainCutoff = int(econData.shape[0] * 0.8)
+    trainCutoff = int(econData.shape[0] * 0.8) 
     trainDf, testDf = econData.iloc[:trainCutoff], econData.iloc[trainCutoff:]
     xTrain, yTrain = trainDf.loc[:, trainDf.columns != "AnnualizedMoM-CPI-Inflation"], trainDf.loc[:, trainDf.columns == "AnnualizedMoM-CPI-Inflation"]
     xTest, yTest = testDf.loc[:, testDf.columns != "AnnualizedMoM-CPI-Inflation"], testDf.loc[:, trainDf.columns == "AnnualizedMoM-CPI-Inflation"]
     return xTrain, yTrain, xTest, yTest
 
-def trainLR(econData):
+def trainLR():
     myLR = LinearRegression()
-    xTrain, yTrain, xTest, yTest = makeTrainTest(econData)
+    xTrain, yTrain, xTest, yTest = makeTrainTestOld()
     myLR.fit(xTrain, yTrain)
     print("Finished training LR")
     # reference: https://towardsdatascience.com/explainable-ai-xai-with-shap-regression-problem-b2d63fdca670
@@ -57,9 +80,9 @@ def evaluateLR(xTest, yTest, myLR):
     print("LR R^2: ", r2_score(yTest, predictions))
     print("LR Adjusted R^2: ", 1 - (1-r2_score(yTest, predictions)) * (len(yTest)-1)/(len(yTest)-xTest.shape[1]-1))
 
-def trainRF(econData):
+def trainRF():
     myRF = RandomForestRegressor()
-    xTrain, yTrain, xTest, yTest = makeTrainTest(econData)
+    xTrain, yTrain, xTest, yTest = makeTrainTestOld()
     myRF.fit(xTrain, yTrain.values.ravel())
     print("Finished training RF")
     # reference: https://towardsdatascience.com/explainable-ai-xai-with-shap-regression-problem-b2d63fdca670
@@ -79,7 +102,7 @@ def evaluateRF(xTest, yTest, myRF):
     print("RF R^2: ", r2_score(yTest, predictions))
     print("RF Adjusted R^2: ", 1 - (1-r2_score(yTest, predictions)) * (len(yTest)-1)/(len(yTest)-xTest.shape[1]-1))
 
-def trainNN(econData):
+def trainNN():
     myNN = keras.Sequential([
     layers.BatchNormalization(),
     layers.Dense(300, activation='relu', input_shape=[15]),
@@ -90,13 +113,12 @@ def trainNN(econData):
     layers.BatchNormalization(),
     layers.Dense(1),
     ])
-
     myNN.compile(
     optimizer='adam',
     loss='mse',
     )   
     es = EarlyStopping(monitor='val_loss', mode='min', verbose=1)
-    xTrain, yTrain, xTest, yTest = makeTrainTest(econData)
+    xTrain, yTrain, xTest, yTest = makeTrainTestOld()
     history = myNN.fit(
     xTrain, yTrain,
     validation_data=(xTest, yTest),
@@ -106,27 +128,66 @@ def trainNN(econData):
     callbacks=[es]
     )
     print("Finished training NN")
+    '''
+    explainer = shap.KernelExplainer(myNN.predict, xTrain)
+    shap_values = explainer.shap_values(xTrain, nsamples = "auto")
+    shap.summary_plot(shap_values, xTrain, feature_names=xTrain.columns, plot_type="bar", show=False)
+    plt.title("Feature Importance for NN")
+    plt.gcf().canvas.manager.set_window_title("Feature Importance for NN")
+    plt.gcf().set_size_inches(10,6)
+    plt.show()
+    '''
     return xTest, yTest, myNN
 
 def evaluateNN(xTest, yTest, myNN):
     test_loss = myNN.evaluate(xTest, yTest, verbose=0)
     # print(myNN.metrics_names)
     print("NN Loss: ", test_loss)
+    predictions = myNN.predict(xTest)
+    print("NN MSE: ", mean_squared_error(yTest, predictions))
+    plt.plot(range(len(yTest)), yTest, color='blue')
+    plt.plot(range(len(predictions)), predictions, color='red')
+    plt.legend(['Actual', 'Predicted'], loc='upper left')
+    plt.title("NN Predictions vs Actual")
+    plt.show()
 
+def trainEvalLasso():
+    myLasso = Lasso(alpha = 0.5, warm_start=True)
+    pre2020xTrain, pre2020yTrain, pre2020xTest, pre2020yTest, post2020xTrain, post2020yTrain, post2020xTest, post2020yTest = makeTrainTest()
+    # First, fit on pre2020 and evaluate
+    myLasso.fit(pre2020xTrain, pre2020yTrain)
+    print("Lasso finished first fit")
+    evaluateLasso(pre2020xTest, pre2020yTest, myLasso, True)
+    # now, retrain with the "new" post2020 data
+    myLasso.fit(post2020xTrain, post2020yTrain)
+    print("Lasso finished second fit")
+    # now, evaluate in total (full test set)
+    xTest = pd.concat([pre2020xTest, post2020xTest])
+    yTest = pd.concat([pre2020yTest, post2020yTest])
+    evaluateLasso(xTest, yTest, myLasso, False)
+
+def evaluateLasso(xTest, yTest, myLasso, pre):
+    predictions = myLasso.predict(xTest)
+    if pre:
+        print("Pre2020 Lasso MSE: ", mean_squared_error(yTest, predictions))
+        print("Pre2020 Lasso R^2: ", r2_score(yTest, predictions))
+        print("Pre2020 Lasso Adjusted R^2: ", 1 - (1-r2_score(yTest, predictions)) * (len(yTest)-1)/(len(yTest)-xTest.shape[1]-1))
+    else:
+        print("Total Lasso MSE: ", mean_squared_error(yTest, predictions))
+        print("Total Lasso R^2: ", r2_score(yTest, predictions))
+        print("Total Lasso Adjusted R^2: ", 1 - (1-r2_score(yTest, predictions)) * (len(yTest)-1)/(len(yTest)-xTest.shape[1]-1))
 
 def main():
-    # Read in the full econ data file
-    econData = readEconData("Data\EconomicData\ALLECONDATA.xlsx")
-    correlationMatrix = econData.corr()
-    correlationMatrix.to_excel("Data\CorrelationMatrix.xlsx")
+    # Try basic Lasso on the econ data
+    trainEvalLasso()
     # Try basic LR on the econ data
-    xTest, yTest, firstLR = trainLR(econData)
+    xTest, yTest, firstLR = trainLR()
     evaluateLR(xTest, yTest, firstLR)
     # Try basic RF on the econ data
-    xTest, yTest, firstRF = trainRF(econData)
+    xTest, yTest, firstRF = trainRF()
     evaluateRF(xTest, yTest, firstRF)
     # Try basic NN on the econ data
-    xTest, yTest, firstNN = trainNN(econData)
+    xTest, yTest, firstNN = trainNN()
     evaluateNN(xTest, yTest, firstNN)
     print("Program Done")
 
---
+++ makeDataset.py
@@ -0,0 +1,42 @@
+''' 
+Create dataset with all the lags for the econ data
+This file makes working in main.py easier
+This file is not meant to do the train/test split or scaling, 
+that is still to be done in main.py
+'''
+import pandas as pd
+
+def readEconData(filename):
+    df = pd.read_excel(filename)
+    # Last number in HousingPriceInd missing so dropping that row
+    df.dropna(subset=["HPI%Change"], inplace=True)
+    return df
+
+def makeLags(df, columnName, lags):
+    for lag in lags:
+        df[columnName + "Lag" + str(lag)] = df[columnName].shift(lag)
+    df.drop(columnName, axis=1, inplace=True)
+    return df
+
+def mainDatasetMaking():
+    # Read in data
+    df = readEconData("Data\EconomicData\ALLECONDATA.xlsx")
+    # Drop the BananaPrice%Change, BreadPrice%Change, EggPrice%Change, and GroundBeefPrice%Change columns from the df
+    # For full reasoning see the partialAutoCorrelationPlots folder, but in sumnmary doing partial autocorrelations on
+    # these columns showed that these columns are likely white noise and do not add much value to the models
+    df.drop(["BananaPrice%Change", "BreadPrice%Change", "EggPrice%Change", "GroundBeefPrice%Change", "2008-9RecessionDummyVar"], axis=1, inplace=True)
+    lagsDict = {"ChickenPrice%Change": [1], "ElectricityPrice%Change": [1,4], "GasolinePrice%Change": [1,2], "HouseStart%Change": [1],
+                "HPI%Change": [1,2,3], "IndPro%Change": [1,2], "MichInflationExpectation": [1], "MilkPrice%Change":[1], "RentalPriceAvg%Change":[1,2,7],
+                "UtilityPrice%Change": [1]}
+    for colName, lags in lagsDict.items():
+        df = makeLags(df, colName, lags)
+    # Move the AnnualizedMoM-CPI-Inflation column to the end of the df for formatting purposes
+    df = df[[c for c in df if c not in ["AnnualizedMoM-CPI-Inflation"]] + ["AnnualizedMoM-CPI-Inflation"]]
+    # Drop all rows with nan values from the df
+    df.dropna(inplace=True)
+    # Save the df to a csv file
+    df.to_excel("Data\ConstructedDataframes\ALLECONDATAwithLags.xlsx", index=False)
+    print("Done making and saving dataset")
+
+if __name__ == "__main__":
+    mainDatasetMaking()
