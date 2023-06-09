e72228dc4c3d2fd8929adcf68ce3b438bf912a6f
Zakariyya Scavotto <44652622+ZakariyyaScavotto@users.noreply.github.com>
2023-06-09 15:58:19 -0400

    Reworked metric and plot outputs for LR

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def readEconData(filename):
 
 def makeTrainTest(): # Train test but with breaking up between pre-2020 and 2020->beyond
     # Read econ data
-    econData = readEconData("Data\ConstructedDataframes\SigLagsAndMichAndCOVID.xlsx")
+    econData = readEconData("Data\ConstructedDataframes\ALLECONDATAwithLagsAndCOVIDData.xlsx")
     # split into pre-2020 and 2020->beyond
     ind2020 = econData.query("Date == '2020-01-01'").index.values[0]
     pre2020EconData, post2020EconData = econData.iloc[:ind2020].copy(), econData.iloc[ind2020:].copy()
@@ -55,34 +55,87 @@ def plotPredictions(xTest, yTest, model, modelName):
     plt.gcf().canvas.manager.set_window_title("Predictions vs Actual for "+modelName)
     plt.show()
 
-def trainLR():
+def getModelMetrics(x, y, model, modelName, training=True):
+    # Get the R^2, Adjusted R^2, MSE, RMSE, MAE, and Pearson's Correlation Coefficent for the model
+    '''temp fix?'''
+    if training==False:
+        y = np.array([value[0] for value in y.values.tolist()])
+    predictions = model.predict(x)
+    r2 = r2_score(y, predictions)
+    adjR2 = 1 - (1-r2)*(len(y)-1)/(len(y)-x.shape[1]-1)
+    mse = mean_squared_error(y, predictions)
+    rmse = np.sqrt(mse)
+    mae = np.mean(np.abs(predictions - y))
+    corr = np.corrcoef(predictions.T, y.T)[0,1]
+    if training:
+        print("Training Metrics for "+modelName+":")
+        print("R^2: "+str(r2))
+        print("Adjusted R^2: "+str(adjR2))
+    else:
+        print("Testing Metrics for "+modelName+":")
+    print("MSE: "+str(mse))
+    print("RMSE: "+str(rmse))
+    print("MAE: "+str(mae))
+    print("Pearson's Correlation Coefficent: "+str(corr))
+    if modelName ==  "LR" and training:
+        printLRCoeffSig(x, y.values.tolist(), model, x.columns)
+        plotLRResiduals(x, y.values.tolist(), model)
+        print("Displayed LR residuals plot for training data")
+        getShapPlot(x, model, modelName)
+        print("Displayed LR SHAP plot for training data")
+    elif modelName == "LR" and not training:
+        plotLRResiduals(x, y.tolist(), model)
+        print("Displayed LR residuals plot for testing data")
+    elif modelName == "RF" and training:
+        getShapPlot(x, model, modelName)
+        print("Displayed RF SHAP plot for training data")
+    return r2, adjR2, mse, rmse, mae, corr
+
+def getShapPlot(x, model, modelName):
+    # reference: https://towardsdatascience.com/explainable-ai-xai-with-shap-regression-problem-b2d63fdca670
+    if modelName == "LR":
+        explainer = shap.explainers.Linear(model, x)
+        shap_values = explainer.shap_values(x)
+        shap.summary_plot(shap_values, x, feature_names=x.columns, plot_type="bar", show=False)
+        plt.title("Feature Importance for Linear Regression")
+        plt.gcf().canvas.manager.set_window_title("Feature Importance for Linear Regression")
+        plt.gcf().set_size_inches(10,6)
+        plt.show()
+    elif modelName == "RF":
+        explainer = shap.TreeExplainer(model)
+        shap_values = explainer.shap_values(x)
+        shap.summary_plot(shap_values, x, feature_names=x.columns, plot_type="bar", show=False)
+        plt.title("Feature Importance for Random Forest")
+        plt.gcf().canvas.manager.set_window_title("Feature Importance for Random Forest")
+        plt.gcf().set_size_inches(10,6)
+        plt.show()
+    else:
+        print("Shap Plot Error: Unsupported model to get SHAP Plot")
+
+def trainEvalLR():
     myLR = LinearRegression()
     pre2020xTrain, pre2020yTrain, pre2020xTest, pre2020yTest, post2020xTrain, post2020yTrain, post2020xTest, post2020yTest = makeTrainTest()
     # combine the xTrains into one dataframe, the xTests into one dataframe, the yTrains into one dataframe, and the yTests into one dataframe
     xTrain, xTest, yTrain, yTest = pd.concat([pre2020xTrain, post2020xTrain]), pd.concat([pre2020xTest, post2020xTest]), pd.concat([pre2020yTrain, post2020yTrain]), pd.concat([pre2020yTest, post2020yTest])
     myLR.fit(xTrain, yTrain)
     print("Finished training LR")
-    # reference: https://towardsdatascience.com/explainable-ai-xai-with-shap-regression-problem-b2d63fdca670
-    explainer = shap.explainers.Linear(myLR, xTrain)
-    shap_values = explainer.shap_values(xTrain)
-    shap.summary_plot(shap_values, xTrain, feature_names=xTrain.columns, plot_type="bar", show=False)
-    plt.title("Feature Importance for Linear Regression")
-    plt.gcf().canvas.manager.set_window_title("Feature Importance for Linear Regression")
-    plt.gcf().set_size_inches(10,6)
-    plt.show()
-    print("LR stats summary:")
-    printLRCoeffSig(xTrain, yTrain.values.tolist(), myLR, xTrain.columns)
-    plotLRResiduals(xTrain, yTrain.values.tolist(), myLR)
-    return xTest, yTest, myLR
-
-def evaluateLR(xTest, yTest, myLR):
-    # Evaluate the LR by getting predictions on xTest, then calculating the MSE and R^2
-    predictions = myLR.predict(xTest)
-    print("LR MSE: ", mean_squared_error(yTest, predictions))
-    print("LR R^2: ", r2_score(yTest, predictions))
-    print("LR Adjusted R^2: ", 1 - (1-r2_score(yTest, predictions)) * (len(yTest)-1)/(len(yTest)-xTest.shape[1]-1))
-    plotPredictions(xTest, yTest, myLR, "Linear Regression")
+    getModelMetrics(xTrain, yTrain, myLR, "LR", training=True)
+    print("Finished displaying training metrics for LR")
+    getModelMetrics(xTest, yTest, myLR, "LR", training=False)
+    print("Finished displaying testing metrics for LR")
     
+def printLRCoeffSig(xTrain, yTrain, LR, xColumns):
+    yTrain = [arr[0] for arr in yTrain]
+    yTrain = np.array(yTrain)
+    LR.coef_ = LR.coef_[0]
+    LR.intercept_ = LR.intercept_[0]
+    print(stats.summary(LR, xTrain, yTrain, xColumns))
+
+def plotLRResiduals(xTrain, yTrain, LR):
+    if any(isinstance(el, np.ndarray) or isinstance(el, list) for el in yTrain):
+        yTrain = [arr[0] for arr in yTrain]
+    yTrain = np.array(yTrain)
+    plots.plot_residuals(LR, xTrain, yTrain, r_type="standardized")
 
 def trainEvalRF():
     myRF = RandomForestRegressor(warm_start=True)
@@ -99,16 +152,9 @@ def trainEvalRF():
     yTest = pd.concat([pre2020yTest, post2020yTest])
     plotPredictions(xTest, yTest, myRF, "Random Forest Full Test Set")
     evaluateRF(xTest, yTest, myRF)
-    # reference: https://towardsdatascience.com/explainable-ai-xai-with-shap-regression-problem-b2d63fdca670
     # combine the xTrains into one dataframe to get shap values
     xTrain = pd.concat([pre2020xTrain, post2020xTrain])
-    explainer = shap.TreeExplainer(myRF)
-    shap_values = explainer.shap_values(xTrain)
-    shap.summary_plot(shap_values, xTrain, feature_names=xTrain.columns, plot_type="bar", show=False)
-    plt.title("Feature Importance for Random Forest")
-    plt.gcf().canvas.manager.set_window_title("Feature Importance for Random Forest")
-    plt.gcf().set_size_inches(10,6)
-    plt.show()
+    
 
 def evaluateRF(xTest, yTest, myRF):
     # Evaluate the RF by getting predictions on xTest, then calculating the MSE and R^2
@@ -191,33 +237,16 @@ def evaluateLasso(xTest, yTest, myLasso, pre):
         print("Total Lasso Adjusted R^2: ", 1 - (1-r2_score(yTest, predictions)) * (len(yTest)-1)/(len(yTest)-xTest.shape[1]-1))
         plotPredictions(xTest, yTest, myLasso, "Lasso Full Test Set")
 
-def printLRCoeffSig(xTrain, yTrain, LR, xColumns):
-    yTrain = [arr[0] for arr in yTrain]
-    yTrain = np.array(yTrain)
-    # print("X shape: ", xTrain.shape)
-    # print("XCols shape: ", xColumns.shape)
-    LR.coef_ = LR.coef_[0]
-    LR.intercept_ = LR.intercept_[0]
-    # print(LR.coef_.shape)
-    # print(LR.intercept_.shape)
-    print(stats.summary(LR, xTrain, yTrain, xColumns))
-
-def plotLRResiduals(xTrain, yTrain, LR):
-    yTrain = [arr[0] for arr in yTrain]
-    yTrain = np.array(yTrain)
-    plots.plot_residuals(LR, xTrain, yTrain, r_type="standardized")
-
 def main():
-    # Try basic Lasso on the econ data
-    trainEvalLasso()
     # Try basic LR on the econ data
-    xTest, yTest, firstLR = trainLR()
-    evaluateLR(xTest, yTest, firstLR)
+    trainEvalLR()
     # Try basic RF on the econ data
     trainEvalRF()
     # Try basic NN on the econ data
     xTest, yTest, firstNN = trainNN()
     evaluateNN(xTest, yTest, firstNN)
+    # Try basic Lasso on the econ data
+    trainEvalLasso()
     print("Program Done")
 
 if __name__ == "__main__":
