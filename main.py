5c94de3c1cf4660a78217705dd50613c87e9370c
Zakariyya Scavotto <44652622+ZakariyyaScavotto@users.noreply.github.com>
2023-06-30 19:26:28 -0400

    RNN timestep method finished, retrained with the 12 month timestep, started implementing new train/test split

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ from sklearn.linear_model import Lasso
 from regressors import stats, plots
 import shutil, os
 import pickle
+from functools import partial
 
 def readEconData(filename):
     return pd.read_excel(filename)
@@ -181,8 +182,8 @@ def trainEvalLR(loadModel=False):
 def printLRCoeffSig(xTrain, yTrain, LR, xColumns):
     yTrain = [arr[0] for arr in yTrain]
     yTrain = np.array(yTrain)
-    # LR.coef_ = LR.coef_[0]
-    # LR.intercept_ = LR.intercept_[0]
+    LR.coef_ = LR.coef_[0]
+    LR.intercept_ = LR.intercept_[0]
     print(stats.summary(LR, xTrain, yTrain, xColumns))
 
 def plotLRResiduals(xTrain, yTrain, LR):
@@ -251,7 +252,7 @@ def newTrainEvalNN(loadModel=False):
         tuner = RandomSearch(
         buildNN,
         objective = 'val_loss',
-        max_trials = 10, # 300
+        max_trials = 300, # 300
         executions_per_trial = 3, #3
         directory = "nnProject",
         project_name = "NN"
@@ -298,7 +299,7 @@ def trainEvalRNN(loadModel=False):
     pre2020xTrain, pre2020yTrain, pre2020xTest, pre2020yTest, post2020xTrain, post2020yTrain, post2020xTest, post2020yTest = makeTrainTest("RNN")
     xTrain, xTest, yTrain, yTest = pd.concat([pre2020xTrain, post2020xTrain]), pd.concat([pre2020xTest, post2020xTest]), pd.concat([pre2020yTrain, post2020yTrain]), pd.concat([pre2020yTest, post2020yTest])
     # xTrain, xTest = xTrain.values.reshape(-1, 1, 24), xTest.values.reshape(-1, 1, 24) # reshape train/test data for RNN to work (adding time dimension)
-    timestep = 3 # number of timesteps to look back
+    timestep = 12 # number of timesteps to look back
     # Split the xTrain and xTeset into rolling windows of size timestep, and have yTrain and yTest be the next value
     rnnXTrain = np.array([xTrain[i:i+timestep] for i in range(len(xTrain)-timestep)])
     rnnXTest = np.array([xTest[i:i+timestep] for i in range(len(xTest)-timestep)])
@@ -317,7 +318,7 @@ def trainEvalRNN(loadModel=False):
         tuner = RandomSearch(
         buildRNN,
         objective = 'val_loss',
-        max_trials = 10, #  300
+        max_trials = 300, #  300
         executions_per_trial = 3,
         directory = "rnnProject",
         project_name = "RNN"
@@ -346,25 +347,72 @@ def trainEvalRNN(loadModel=False):
 
 def buildRNN(hp):
     myRNN = keras.Sequential()
-    myRNN.add(layers.SimpleRNN(units = hp.Int('units', 1, 60), activation='relu', input_shape=(3,23), dropout = (hp.Float('dropout', 0.2, 0.7, step=0.05)), recurrent_dropout = (hp.Float('recurDropout' , 0.2, 0.7, step=0.05)),return_sequences=False))
+    myRNN.add(layers.SimpleRNN(units = hp.Int('units', 1, 60), activation='relu', input_shape=(12,23), dropout = (hp.Float('dropout', 0.2, 0.7, step=0.05)), recurrent_dropout = (hp.Float('recurDropout' , 0.2, 0.7, step=0.05)),return_sequences=False))
     myRNN.add(layers.Dense(1))
     myRNN.compile(optimizer=keras.optimizers.Adam(hp.Choice('learning_rate', values=[1e-2, 1e-4])),
                     loss = 'mse', metrics = [metrics.MeanSquaredError(), metrics.MeanAbsoluteError()])
     return myRNN
 
+def buildRNNTime(time, hp):
+    myRNN = keras.Sequential()
+    myRNN.add(layers.SimpleRNN(units = hp.Int('units', 1, 60), activation='relu', input_shape=(time,23), dropout = (hp.Float('dropout', 0.2, 0.7, step=0.05)), recurrent_dropout = (hp.Float('recurDropout' , 0.2, 0.7, step=0.05)),return_sequences=False))
+    myRNN.add(layers.Dense(1))
+    myRNN.compile(optimizer=keras.optimizers.Adam(hp.Choice('learning_rate', values=[1e-2, 1e-4])),
+                    loss = 'mse', metrics = [metrics.MeanSquaredError(), metrics.MeanAbsoluteError()])
+    return myRNN
+
+def determineRNNTimestep():
+    pre2020xTrain, pre2020yTrain, pre2020xTest, pre2020yTest, post2020xTrain, post2020yTrain, post2020xTest, post2020yTest = makeTrainTest("RNN")
+    xTrain, xTest, yTrain, yTest = pd.concat([pre2020xTrain, post2020xTrain]), pd.concat([pre2020xTest, post2020xTest]), pd.concat([pre2020yTrain, post2020yTrain]), pd.concat([pre2020yTest, post2020yTest])
+    # xTrain, xTest = xTrain.values.reshape(-1, 1, 24), xTest.values.reshape(-1, 1, 24) # reshape train/test data for RNN to work (adding time dimension)
+    bestTimestep = -1
+    bestRMSE= 999999.9999999999
+    for timestep in range(3, 13):
+        if os.path.exists("rnnProject"):
+            shutil.rmtree("rnnProject")
+            print("Old RNN project deleted")
+        # Split the xTrain and xTeset into rolling windows of size timestep, and have yTrain and yTest be the next value
+        rnnXTrain = np.array([xTrain[i:i+timestep] for i in range(len(xTrain)-timestep)])
+        rnnXTest = np.array([xTest[i:i+timestep] for i in range(len(xTest)-timestep)])
+        rnnYTrain = np.array([yTrain.values[i+timestep] for i in range(len(yTrain)-timestep)])
+        rnnYTest = np.array([yTest.values[i+timestep] for i in range(len(yTest)-timestep)])
+        rnnXTrain.reshape(len(rnnXTrain), timestep, 23)
+        rnnXTest.reshape(len(rnnXTest), timestep, 23)
+        # buildRNNModel = partial(buildRNN, time=timestep)
+        tuner = RandomSearch(
+        lambda hp : buildRNNTime(timestep, hp),
+        objective = 'val_loss',
+        max_trials = 200, #  300
+        executions_per_trial = 3,
+        directory = "rnnProject",
+        project_name = "RNN"
+        )
+        # print(tuner.search_space_summary())
+        tuner.search(rnnXTrain, rnnYTrain, epochs=100, validation_data=(rnnXTest, rnnYTest))
+        print(tuner.results_summary())
+        # es = EarlyStopping(monitor='val_loss', mode='min', verbose=1)
+        myRNN = tuner.hypermodel.build(tuner.get_best_hyperparameters()[0])
+        history = myRNN.fit(
+        rnnXTrain, rnnYTrain,
+        validation_data=(rnnXTest, rnnYTest),
+        batch_size=32,
+        epochs=100,
+        verbose=0, # decided to make verbose to follow the training, feel free to set to 0
+        #callbacks=[es]
+        )
+        print("Finished training RNN")
+        trainR2, trainAdjR2, trainMSE, trainRMSE, trainMAE, trainCorr = getModelMetrics(rnnXTrain, rnnYTrain, myRNN, "RNN", training=True)
+        testR2, testAdjR2, testMSE, testRMSE, testMAE, testCorr = getModelMetrics(rnnXTest, rnnYTest, myRNN, "RNN", training=False)
+        if testRMSE < bestRMSE:
+            bestRMSE = testRMSE
+            bestTimestep = timestep
+    print("BEST RNN TIMESTEP: ", bestTimestep)
+
 def main():
-    loadModel = True
-    '''
-    # Try  LR on the data
-    trainEvalLR(loadModel)
-    # Try  RF on the data
-    trainEvalRF(loadModel)
-    # Try NN on the data
-    newTrainEvalNN(loadModel)
-    # Try RNN on the data
-    trainEvalRNN(loadModel)'''
+    loadModel = False
     metricsDict = {"LR": trainEvalLR(loadModel), "RF": trainEvalRF(loadModel), "NN": newTrainEvalNN(loadModel), "RNN": trainEvalRNN(loadModel)}
     compileMetrics(metricsDict)
+    # determineRNNTimestep()
     print("Program Done")
 
 if __name__ == "__main__":
